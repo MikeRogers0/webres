92aec3c6d256fdd4ba6c8632109d0a847e3ff533
Mike Rogers <mike.r@fullondesign.co.uk>
2012-12-07 17:46:37 +0000

    adding in css files and blank js objects

--- httpdocs/js/main.js
+++ httpdocs/js/main.js
@@ -1,6 +1,6 @@
 /* Lets put the JS stuff here */
 
-head.js("js/dijkstras.js", function() {
+head.js("js/objects/dijkstras.js", 'js/objects/terrain.js', 'js/objects/weather.js', function() {
  
    // all done
  
